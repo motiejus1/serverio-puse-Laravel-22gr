ec680c0c850ecb7590814df64d70b4e56c52e4ea
motiejus1 <motiejus.baliukonis@vka.lt>
2022-03-17 19:53:45 +0200

    *update show

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Validator;
 
 class ClientController extends Controller
 {
@@ -36,25 +37,51 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        $client = new Client;
         
-        // $client = Client::create([
-        //     'name' => $request->client_name,
-        //     'surname' => $request->client_surname,
-        //     'description' => $request->client_description
-        // ]);
+        $input = [
+            'client_name'=> $request->client_name,
+            'client_surname'=> $request->client_surname,
+            'client_description'=> $request->client_description,
+        ];
 
-        $client->name = $request->client_name;
-        $client->surname = $request->client_surname;
-        $client->description = $request->client_description;
+        $rules = [
+            'client_name'=> 'required',
+            'client_surname'=> 'required',
+            'client_description'=> 'required',
+        ];
+   
+        $validator = Validator::make($input, $rules);
 
-        $client->save();
 
-        return response()->json(array(
-            'success' => 'Client added',
-            'name' => $client->name,
-            'surname' => $client->surname
-        ));
+        if($validator->fails()) {
+
+            //zinuciu masyva, kuriose surasyta viskas, kas negerai
+            //atvaizduoti zinuciu masyva prie kiekvieno input laukelio
+            $errors = $validator->messages()->get('*'); //pasiima visu ivykusiu klaidu sarasa
+            $client_array = array(
+                'errorMessage' => "validator fails",
+                'errors' => $errors
+            );
+        } else {
+
+            $client = new Client;
+            $client->name = $request->client_name;
+            $client->surname = $request->client_surname;
+            $client->description = $request->client_description;
+        
+            $client->save();//po isaugojimo momento
+
+            $client_array = array(
+                'successMessage' => "Client stored succesfuly",
+                'clientId' => $client->id,
+                'clientName' => $client->name,
+                'clientSurname' => $client->surname,
+                'clientDescription' => $client->description,
+            );
+    }
+
+        $json_response =response()->json($client_array); 
+        return $json_response;
     }
 
     /**
@@ -65,7 +92,9 @@ class ClientController extends Controller
      */
     public function show($id)
     {
-        //
+        $client = Client::find($id);
+
+        return response()->json($client);
     }
 
     /**
@@ -75,9 +104,56 @@ class ClientController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    //Client $client => id
     public function update(Request $request, $id)
     {
-        //
+        //$client is not defined
+        //$id
+
+        $input = [
+            'client_name'=> $request->client_name,
+            'client_surname'=> $request->client_surname,
+            'client_description'=> $request->client_description,
+        ];
+
+        $rules = [
+            'client_name'=> 'required',
+            'client_surname'=> 'required',
+            'client_description'=> 'required',
+        ];
+   
+        $validator = Validator::make($input, $rules);
+
+
+        if($validator->fails()) {
+
+            //zinuciu masyva, kuriose surasyta viskas, kas negerai
+            //atvaizduoti zinuciu masyva prie kiekvieno input laukelio
+            $errors = $validator->messages()->get('*'); //pasiima visu ivykusiu klaidu sarasa
+            $client_array = array(
+                'errorMessage' => "validator fails",
+                'errors' => $errors
+            );
+        } else {
+
+            $client = Client::find($id);//find suranda irasa pagal id
+            $client->name = $request->client_name;
+            $client->surname = $request->client_surname;
+            $client->description = $request->client_description;
+        
+            $client->save();//po isaugojimo momento
+
+            $client_array = array(
+                'successMessage' => "Client stored succesfuly",
+                'clientId' => $client->id,
+                'clientName' => $client->name,
+                'clientSurname' => $client->surname,
+                'clientDescription' => $client->description,
+            );
+    }
+
+        $json_response =response()->json($client_array); 
+        return $json_response;
     }
 
     /**
